770129b76e1139524a0b72e23573f52c4eb524f7
alemagno10 <alemagno03@gmail.com>
2024-04-11 19:53:27 -0300

    update

--- src/main/java/insper/store/DiscoveryApplication.java
+++ src/main/java/insper/store/DiscoveryApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-// import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @EnableEurekaServer
 @SpringBootApplication
-// @EnableSwagger2
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
